d31f7a36c91831e3dadb1cb200f56138be59129e
Robson <robsong200@gmail.com>
2020-06-24 17:30:54 -0300

    Obtendo um post por id

---
+++ src/main/java/com/robson/springbootmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.robson.springbootmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.robson.springbootmongo.domain.Post;
+import com.robson.springbootmongo.repositories.PostRepository;
+import com.robson.springbootmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
