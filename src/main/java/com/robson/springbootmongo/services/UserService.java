0dedc38f931aba9b26024a8527aba83d78776b0c
Robson <robsong200@gmail.com>
2020-06-24 14:07:33 -0300

    Insercao de usuario com POST

--- src/main/java/com/robson/springbootmongo/services/UserService.java
+++ src/main/java/com/robson/springbootmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.robson.springbootmongo.domain.User;
+import com.robson.springbootmongo.dto.UserDTO;
 import com.robson.springbootmongo.repositories.UserRepository;
 import com.robson.springbootmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
